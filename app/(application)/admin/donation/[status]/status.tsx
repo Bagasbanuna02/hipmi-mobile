73a803f2e8354f34a6547fdf93a7d937163175ab
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-10-27 10:49:31 +0800

    Update new github

--- app/(application)/admin/donation/[status]/status.tsx
+++ app/(application)/admin/donation/[status]/status.tsx
@@ -1,69 +1,117 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
-    ActionIcon,
-    SearchInput,
-    Spacing,
-    TextCustom,
-    ViewWrapper
+  ActionIcon,
+  LoaderCustom,
+  SearchInput,
+  Spacing,
+  StackCustom,
+  TextCustom,
+  ViewWrapper,
 } from "@/components";
 import AdminComp_BoxTitle from "@/components/_ShareComponent/Admin/BoxTitlePage";
 import AdminTitleTable from "@/components/_ShareComponent/Admin/TableTitle";
 import AdminTableValue from "@/components/_ShareComponent/Admin/TableValue";
+import AdminTitlePage from "@/components/_ShareComponent/Admin/TitlePage";
 import { ICON_SIZE_BUTTON } from "@/constants/constans-value";
+import { apiAdminDonation } from "@/service/api-admin/api-admin-donation";
 import { Octicons } from "@expo/vector-icons";
-import { router, useLocalSearchParams } from "expo-router";
+import { router, useFocusEffect, useLocalSearchParams } from "expo-router";
 import _ from "lodash";
+import { useState, useCallback } from "react";
 import { Divider } from "react-native-paper";
 
 export default function AdminDonationStatus() {
   const { status } = useLocalSearchParams();
+  console.log("[STATUS]", status);
+
+  const [data, setData] = useState<any | null>(null);
+  const [search, setSearch] = useState<string>("");
+  const [loadData, setLoadData] = useState<boolean>(false);
+
+  useFocusEffect(
+    useCallback(() => {
+      onLoadData();
+    }, [status, search])
+  );
+
+  const onLoadData = async () => {
+    try {
+      setLoadData(true);
+      const response = await apiAdminDonation({
+        category: status as "publish" | "review" | "reject",
+        search,
+      });
+
+      console.log("[RES]", JSON.stringify(response, null, 2));
+
+      if (response.success) {
+        setData(response.data);
+      }
+    } catch (error) {
+      console.log("[ERROR]", error);
+      setData([]);
+    } finally {
+      setLoadData(false);
+    }
+  };
+
   const rightComponent = (
     <SearchInput
       containerStyle={{ width: "100%", marginBottom: 0 }}
       placeholder="Cari"
+      value={search}
+      onChangeText={(value) => setSearch(value)}
     />
   );
   return (
     <>
-      <ViewWrapper
-        headerComponent={
+      <ViewWrapper headerComponent={<AdminTitlePage title="Donasi" />}>
+        <StackCustom gap={"sm"}>
           <AdminComp_BoxTitle
-            title={`Donasi ${_.startCase(status as string)}`}
+            title={`${_.startCase(status as string)}`}
             rightComponent={rightComponent}
           />
-        }
-      >
-        <AdminTitleTable
-          title1="Aksi"
-          title2="Username"
-          title3="Judul Donasi"
-        />
-        <Spacing />
-        <Divider />
-
-        {Array.from({ length: 10 }).map((_, index) => (
-          <AdminTableValue
-            key={index}
-            value1={
-              <ActionIcon
-                icon={
-                  <Octicons name="eye" size={ICON_SIZE_BUTTON} color="black" />
-                }
-                onPress={() => {
-                  router.push(`/admin/donation/${index}/${status}`);
-                }}
-              />
-            }
-            value2={<TextCustom truncate={1}>Username username</TextCustom>}
-            value3={
-              <TextCustom truncate={2}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Blanditiis asperiores quidem deleniti architecto eaque et
-                nostrum, ad consequuntur eveniet quisquam quae voluptatum
-                ducimus! Dolorem nobis modi officia debitis, beatae mollitia.
-              </TextCustom>
-            }
+          <AdminTitleTable
+            title1="Aksi"
+            title2="Username"
+            title3="Judul Donasi"
           />
-        ))}
+          <Divider />
+
+          {loadData ? (
+            <LoaderCustom />
+          ) : _.isEmpty(data) ? (
+            <TextCustom align="center" size="small" color="gray">
+              Belum ada data
+            </TextCustom>
+          ) : (
+            data?.map((item: any, index: number) => (
+              <AdminTableValue
+                key={index}
+                value1={
+                  <ActionIcon
+                    icon={
+                      <Octicons
+                        name="eye"
+                        size={ICON_SIZE_BUTTON}
+                        color="black"
+                      />
+                    }
+                    onPress={() => {
+                      router.push(`/admin/donation/${item.id}/${status}`);
+                    }}
+                  />
+                }
+                value2={<TextCustom truncate={1}>{item?.Author?.username || "-"}</TextCustom>}
+                value3={
+                  <TextCustom truncate={2}>
+                    {item?.title || "-"}
+                  </TextCustom>
+                }
+              />
+            ))
+          )}
+        </StackCustom>
       </ViewWrapper>
     </>
   );
--- app/(application)/admin/donation/index.tsx
+++ app/(application)/admin/donation/index.tsx
@@ -1,15 +1,70 @@
 import { Spacing, StackCustom, ViewWrapper } from "@/components";
 import {
-    IconList,
-    IconPublish,
-    IconReject,
-    IconReview,
+  IconList,
+  IconPublish,
+  IconReject,
+  IconReview,
 } from "@/components/_Icon/IconComponent";
 import AdminComp_BoxDashboard from "@/components/_ShareComponent/Admin/BoxDashboard";
 import AdminTitlePage from "@/components/_ShareComponent/Admin/TitlePage";
 import { MainColor } from "@/constants/color-palet";
+import { apiAdminDonation } from "@/service/api-admin/api-admin-donation";
+import { useFocusEffect } from "expo-router";
+import { useState, useCallback } from "react";
 
 export default function AdminDonation() {
+  const [data, setData] = useState<any | null>(null);
+  const [loadData, setLoadData] = useState<boolean>(false);
+
+  useFocusEffect(
+    useCallback(() => {
+      onLoadData();
+    }, [])
+  );
+
+  const onLoadData = async () => {
+    try {
+      setLoadData(true);
+      const response = await apiAdminDonation({
+        category: "dashboard",
+      });
+
+      console.log("[RES]", JSON.stringify(response, null, 2));
+
+      if (response.success) {
+        setData(response.data);
+      }
+    } catch (error) {
+      console.log("[ERROR]", error);
+      setData([]);
+    } finally {
+      setLoadData(false);
+    }
+  };
+
+  const listData = [
+    {
+      label: "Publish",
+      value: (data && data.publish) || 0,
+      icon: <IconPublish size={25} color={MainColor.green} />,
+    },
+    {
+      label: "Review",
+      value: (data && data.review) || 0,
+      icon: <IconReview size={25} color={MainColor.orange} />,
+    },
+    {
+      label: "Reject",
+      value: (data && data.reject) || 0,
+      icon: <IconReject size={25} color={MainColor.red} />,
+    },
+    {
+      label: "Kategori",
+      value: (data && data.categoryDonation) || 0,
+      icon: <IconList size={25} color={MainColor.white_gray} />,
+    },
+  ];
+
   return (
     <>
       <ViewWrapper>
@@ -24,26 +79,3 @@ export default function AdminDonation() {
     </>
   );
 }
-
-const listData = [
-  {
-    label: "Publish",
-    value: 4,
-    icon: <IconPublish size={25} color={MainColor.green} />,
-  },
-  {
-    label: "Review",
-    value: 7,
-    icon: <IconReview size={25} color={MainColor.orange} />,
-  },
-  {
-    label: "Reject",
-    value: 5,
-    icon: <IconReject size={25} color={MainColor.red} />,
-  },
-  {
-    label: "Kategori",
-    value: 4,
-    icon: <IconList size={25} color={MainColor.white_gray} />,
-  },
-];
